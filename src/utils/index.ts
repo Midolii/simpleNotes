81ef8ede304cd0b980b7eb933fd547da2bcffaa8
Midolii <462885536@qq.com>
2023-03-22 21:14:01 +0800

    feat: 添加dateFormater方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,18 @@
-export {};
+import moment from "moment";
+
+const dateFormater = (date: Date | number, formater?: string) => {
+  let newDate = date;
+  if (Object.prototype.toString.call(date).slice(-8, -1) === "Number") {
+    newDate = new Date(date);
+  }
+  if (formater) {
+    try {
+      return moment(newDate).format(formater);
+    } catch {
+      return moment(newDate).format("YYYY-MM-DD HH:mm:ss");
+    }
+  }
+  return moment(newDate).format("YYYY-MM-DD HH:mm:ss");
+};
+
+export { dateFormater };
